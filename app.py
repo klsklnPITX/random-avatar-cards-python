d42cee8d62a2a5d9316b96e36c69431ab9a8c325
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2020-08-30 14:30:51 +0200

    Added dynamic avatar listing from avatar json file

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+import json
 
 app = Flask(__name__)
 
 
+def get_avatars():
+    with open("static/avatar.json") as f:
+        data_avatars = json.load(f)
+    return data_avatars
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    data_avatars = get_avatars()
+    return render_template("index.html", data_avatars=data_avatars)
