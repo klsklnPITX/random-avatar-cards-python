2b0181adf26b67fc7e4723b00ff909b2a2e10e3c
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2020-08-31 12:06:18 +0200

    Deleted js folder

--- static/js/script.js
+++
@@ -1,8 +0,0 @@
-function searchInput(avatars) {
-    var searchBoxInput = document.getElementById("searchBox").value;
-    var avatars_obj = JSON.parse(avatars)
-    const filteredAvatars = avatars_obj.filter(avatars_obj => {
-        return (avatars_obj.name.toLowerCase().includes(searchBoxInput.toLowerCase()))
-    });
-    console.log(avatars);
-}
